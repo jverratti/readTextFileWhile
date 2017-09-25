53b1cdccdc89350950fcc096629fa99bfbc1e37b
jverratti <31357738+jverratti@users.noreply.github.com>
2017-09-25 15:53:17 -0600

    whileIterations

---
+++ readTextFileWhile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace readTextFileWhile
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader myReader = new StreamReader("Values.txt");
+
+            string line = "";
+
+            while(line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null)
+                    Console.WriteLine(line);
+            }
+
+            myReader.Close();
+                    Console.ReadLine();
+
+
+
+        }
+    }
+}
+
+
+///a stream reader works like a straw and sucks in data
+/// null value means that it has unknown value
+/// while iteration while continue to execute until
+/// becomes false
+/// each value of other ta bis retrieved using stream line method
+/// failure to close file can leave reference to use file
